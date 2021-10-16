9c7fffa8fff2cc937648f3faeb39d61c802e02c8
erkutbas <74869943+erkutbas0@users.noreply.github.com>
2021-10-16 10:18:56 +0300

    Initial Commit

---
+++ WeatherApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApplication
+//
+//  Created by Erkut Bas on 16.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
